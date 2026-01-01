50c0564c30f4256faa39d528bb8925ce541a3227
GersonESS <gebhsantos@gmail.com>
2026-01-01 00:49:37 -0300

    Update server.js

--- 01/api/server.js
+++ 01/api/server.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+const app = express();
+app.use(express.json());
+const users = [];
 
-app.get('/', (req, res) => {
-  res.send('Feliz Ano Novo')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/usuarios', (req, res) => {
+
+    res.status(200).json(users);
+   });
+
+app.post('/usuarios', (req, res) => {
+  users.push(req.body);
+  
+  res.status(201).json(req.body);
+  });
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+  });
