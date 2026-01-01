667ac8d5e68796049f7fdf296ceda76a46851526
GersonESS <gebhsantos@gmail.com>
2025-12-31 22:21:42 -0300

    w

--- 01/api/server.js
+++ 01/api/server.js
@@ -1,9 +1,10 @@
-const express = require('express')
+
+import express from 'express'
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Feliz Ano Novo')
+  res.send('Feliz Ano Novo Com import')
 })
 
 app.listen(port, () => {
